b71d0071b058291a2978f540e00291bf66cd40b0
SONIA-CLAUDIA MOLDOVAN <sonia.claudia.moldovan@stud.ubbcluj.ro>
2020-11-07 20:38:38 +0200

    Add project files.

---
+++ Moldovan_Sonia_Lab5/MyPhoneNumbersTableAdapter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Moldovan_Sonia_Lab5.PhoneNumbersDataSetTableAdapters
+{
+	public partial class PhoneNumbersTableAdapter
+	{
+		public PhoneNumbersTableAdapter(String connectionString)
+		{
+			this.ClearBeforeFill = true;
+			this.Connection = new System.Data.SqlClient.SqlConnection(connectionString);
+		}
+	}
+}
